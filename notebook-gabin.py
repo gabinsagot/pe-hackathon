63ebccd13c15cf4457bcd274c78b9bbda5ca47cb
Gabin Sagot <gabin.sagot@etu.minesparis.psl.eu>
2024-10-29 16:01:15 +0100

    Premiere fonction pour les symetries et les rotations

--- notebook-gabin.py
+++ notebook-gabin.py
@@ -7,4 +7,45 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
+
+# %%
+import numpy as np
+import exact_cover_py as ex
+
+# %%
+RAW_SHAPES = {
+    "F": [[1, 1, 0], [0, 1, 1], [0, 1, 0]],
+    "I": [[1, 1, 1, 1, 1]],
+    "L": [[1, 0, 0, 0], [1, 1, 1, 1]],
+    "N": [[1, 1, 0, 0], [0, 1, 1, 1]],
+    "P": [[1, 1, 1], [1, 1, 0]],
+    "T": [[1, 1, 1], [0, 1, 0], [0, 1, 0]],
+    "U": [[1, 1, 1], [1, 0, 1]],
+    "V": [[1, 1, 1], [1, 0, 0], [1, 0, 0]],
+    "W": [[1, 0, 0], [1, 1, 0], [0, 1, 1]],
+    "X": [[0, 1, 0], [1, 1, 1], [0, 1, 0]],
+    "Y": [[0, 1, 0, 0], [1, 1, 1, 1]],
+    "Z": [[1, 1, 0], [0, 1, 0], [0, 1, 1]],
+}
+
+
+# %%
+def symetrie(lettre):
+    L = []
+    l1, l2 = lettre, lettre
+    for j in range(3):
+        l1[j][0]= l1[j][2]
+        l2[0][j] = l2[2][j]
+    L.append(l1)
+    L.append(l2)
+    return L
+
+
+# %%
+F = RAW_SHAPES["F"]
+symetrie(F)
